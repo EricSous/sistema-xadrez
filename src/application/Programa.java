0c407aefe98d4b02302d3b8674daa138c9d9909f
Eric de Sousa Andrade <ericandrade.millenium@poupex.com.br>
2022-01-25 14:55:08 -0300

    Posição das peças

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,7 +1,10 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Programa {
     public static void main(String[] args) {
-        
+        Posicao po = new Posicao(1,3);
+        System.out.println(po);
     }
 }
