c214405352fa856f74f051b934fe931053f14b21
Eric de Sousa Andrade <ericandrade.millenium@poupex.com.br>
2022-01-26 09:16:40 -0300

    Tratamento de excessões

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -7,6 +7,9 @@ public class Tabuleiro {
     private Peca[][] peca;
 
     public Tabuleiro(int linhas, int colunas) {
+        if(linhas<1 || colunas <1) {
+            throw new TabuleiroException("Erro ao criar tabuleiro muito curto");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         peca = new Peca[linhas][colunas];
@@ -17,24 +20,45 @@ public class Tabuleiro {
         return linhas;
     }
 
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
-
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
     public Peca peca(int linhas, int colunas){
+        if(!posicaoExiste(linhas, colunas)) {
+            throw new TabuleiroException("Posição nao esta no tabuleiro");
+        }
         return peca[linhas][colunas];
     }
+    public Peca peca(Posicao posicao){
+        if(!posicaoExiste(posicao)) {
+            throw new TabuleiroException("Posição nao esta no tabuleiro");
+        }
+        return peca[posicao.getLinhas()][posicao.getColuna()];
+    }
 
-        public void colocarPeca(Peca peca, Posicao posicao){
+    public void colocarPeca(Peca peca, Posicao posicao){
+        if(possuiUmaPeca(posicao)){
+            throw new TabuleiroException("Ja possui uma peça nesta posição " + posicao);
+        }
         this.peca[posicao.getLinhas()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
+    }
+
+    public Boolean posicaoExiste(int linhas, int colunas){
+        return linhas>=0 && linhas < this.linhas && colunas>=0 && colunas < this.colunas;
+
+    }
+
+    public Boolean posicaoExiste(Posicao posicao){
+        return posicaoExiste(posicao.getLinhas(), posicao.getColuna());
+
+    }
+
+    public Boolean possuiUmaPeca(Posicao posicao){
+        if(!posicaoExiste(posicao)){
+            throw new TabuleiroException("Posição inexistente no tabuleiro ");
         }
+        return peca(posicao) != null;
+    }
 }
--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -20,7 +20,7 @@ public class PartidaDeXadrez {
         PecaDeXadrez[][] pecaDeXadrez = new PecaDeXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
         for(int i =0; i< tabuleiro.getLinhas(); i++){
             for (int j = 0; j<tabuleiro.getColunas(); j++){
-                pecaDeXadrez[i][j] = (PecaDeXadrez) tabuleiro.peca(i,j);
+                pecaDeXadrez[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j);
             }
         }
         return pecaDeXadrez;
